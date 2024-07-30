59126271919aa5dc1a778dfe34c58cbbb6ef16fc
Larissa Nazario Domingos <121519946+LarissaNazario16do@users.noreply.github.com>
2024-07-30 13:58:37 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ function colocarDadosNaTela(dados) {
     Math.floor(dados.main.temp) + "°C";
   document.querySelector(".texto-previsao").innerHTML =
     dados.weather[0].description;
-  document.querySelector(".umidade").innerHTML = dados.main.humidity + "%";
+  document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
   document.querySelector(
     ".img-previsao"
   ).src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
